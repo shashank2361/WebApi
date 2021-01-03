dc1b1a1f2a4addd7a8432be4c5ceb9b44ebb69f9
DESKTOP-SB3UMDG\Shashank-PC <shashank2361@yahoo.co.in>
2021-01-03 13:50:18 +0000

    added test proj

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,43 @@
+using BLL;
+using DAL;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using WebApi.Controllers;
+using WebApi.Helpers;
+using WebApi.Services;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+
+        EmployeeController _employeeController;
+        private IUserService _userService;
+        IEmployeeBs _employeeBs;
+        IEmployeeDb _employeeDb;
+
+        IOptions<AppSettings> _appSettings;
+
+
+        public UnitTest1()
+        {
+            //_employeeDb = new EmployeeDb();
+            //_employeeBs = new EmployeeBs();
+            //_appSettings = new Options<AppSettings>();
+            //_userService = new UserService();
+
+            //_employeeController = new EmployeeController(_employeeBs);
+
+        }
+        [Fact]
+        public void Test1()
+        {
+           Assert.True(true, $"The test  is not valid");
+
+        }
+
+      
+    }
+}
